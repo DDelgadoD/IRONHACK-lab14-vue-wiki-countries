7589f801f0634f77d779623fb801945e53e37501
David Delgado-Dueñas <david.delgado82@gmail.com>
2022-10-14 17:40:11 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,19 +5,19 @@ import axios from 'axios'
 
 //creamos una instancia de la pokeapi
 // timeout corta la peticion cuando pasa el timepo en ms
-const pokeApi = axios.create({
+const countryApi = axios.create({
     baseURL: 'https://ih-countries-api.herokuapp.com/countries/',
     timeout: 3000
 });
 
 export const getCountry = async (alpha3Code) => {
-    const response = await pokeApi.get(alpha3Code);
+    const response = await countryApi.get(alpha3Code);
     //.data es parecido al .json del fetch pero no es una promesa
     return response.data
 };
 
 export const getCountries = async () => {
-    const response = await pokeApi.get('');
+    const response = await countryApi.get('');
     //.data es parecido al .json del fetch pero no es una promesa
     return response.data
 };
